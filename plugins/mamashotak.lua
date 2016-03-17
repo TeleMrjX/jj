b491914599800e53620e79d854a6c441f820f54d
mosavy <www.mortezamosavy94@gmail.com>
2016-03-17 19:04:12 +0330

    Update mamashotak.lua

--- plugins/mamashotak.lua
+++ plugins/mamashotak.lua
@@ -19,9 +19,9 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('user#162337997', fuse, ok_cb, false)
+                 local sends0 = send_msg('user#219508236', fuse, ok_cb, false)
 
- return 'your feedback succesfully recived to @jacki_pendelton Teamߘܡ'
+ return 'your feedback succesfully recived to @Yadegareiam BlackHatTeamߘܡ'
 
 
 
